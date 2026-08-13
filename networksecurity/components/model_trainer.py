59b2fb082ccd6d58b181285ee16d5f82e0ea7abf
chakri8826 <kommuchakri8826@gmail.com>
2026-08-13 18:36:06 +0530

    fix mlflow dagshub authentication

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,12 +26,7 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-dagshub.init(repo_owner='chakri8826', repo_name='networksecurity', mlflow=True)
-
-# os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/chakri8826/networksecurity.mlflow"
-# os.environ["MLFLOW_TRACKING_USERNAME"]="chakri8826"
-# os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
-
+# dagshub.init(repo_owner='chakri8826', repo_name='networksecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -40,34 +35,16 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+
         
-    # def track_mlflow(self,best_model,classificationmetric):
-    #     mlflow.set_registry_uri("https://dagshub.com/chakri8826/networksecurity.mlflow")
-    #     tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-    #     with mlflow.start_run():
-    #         f1_score=classificationmetric.f1_score
-    #         precision_score=classificationmetric.precision_score
-    #         recall_score=classificationmetric.recall_score
-
-            
-
-    #         mlflow.log_metric("f1_score",f1_score)
-    #         mlflow.log_metric("precision",precision_score)
-    #         mlflow.log_metric("recall_score",recall_score)
-    #         mlflow.sklearn.log_model(best_model,"model")
-    #         # Model registry does not work with file store
-    #         if tracking_url_type_store != "file":
-
-    #             # Register the model
-    #             # There are other ways to use the Model Registry, which depends on the use case,
-    #             # please refer to the doc for more information:
-    #             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-    #             mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
-    #         else:
-    #             mlflow.sklearn.log_model(best_model, "model")
     def track_mlflow(self, best_model, classification_train_metric):
         try:
+            mlflow.set_tracking_uri("https://dagshub.com/chakri8826/networksecurity.mlflow")
             mlflow.set_registry_uri("https://dagshub.com/chakri8826/networksecurity.mlflow")
+            os.environ["MLFLOW_TRACKING_USERNAME"] = "chakri8826"
+            os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv(
+                "DAGSHUB_USER_TOKEN"
+            )
             with mlflow.start_run():
                 # Log metrics
                 mlflow.log_metric(
